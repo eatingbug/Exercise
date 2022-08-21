8a640782e7d288b261ca5f940628661610dfd956
Grooomit <tmd5972@gmail.com>
2022-08-21 09:56:43 +0900

    Section2

---
+++ section2-week3/src/main/java/com/codestates/section2week3/Section2Week3Application.java
@@ -0,0 +1,13 @@
+package section2week3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section2Week3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section2Week3Application.class, args);
+	}
+
+}
---
+++ section2-week3/src/main/java/com/codestates/section2week3/Section2Week3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.section2week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section2Week3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
